0cde68fcbc6da38dc31fb7f61d06211a68de3e49
mrprov12 <mrprov@mac.com>
2021-08-25 13:51:07 -0600

    smartAssigning complete

---
+++ Asana_Interview_Qs/smartAssigning.js
@@ -0,0 +1,24 @@
+
+/*
+I: names -array of strs, statues -array of bool (T = on vacation, closed to tasks), projects - array of num(int?), tasks - array of num(int?)
+O: person with highest availability - str
+C:
+E: all employees status are true
+*/
+
+function smartAssigning(names, statuses, projects, tasks) {
+  //higher availability means tasks <= && projects <
+
+  let HAindex = -1; //highestAvailabilityIndex
+
+  for (var i = 0; i < names.length; i++) { //indexes are synced btw lists
+      if (HAindex === -1 && !statuses[i]) {
+          HAindex = i;
+      } else if (HAindex > -1 && !statuses[i]) {
+          if (tasks[i] < tasks[HAindex] ) {
+          HAindex = i;
+      }
+      }
+  }
+  return names[HAindex];
+}
--- Asana_Interview_Qs/tastType.js
+++ Asana_Interview_Qs/tastType.js
@@ -1,1 +1,26 @@
 //https://app.codesignal.com/company-challenges/asana/2vJMZnQzdkkhCvmxs
+
+function tasksTypes(deadlines, day) {
+  let today = 0;
+  let upcoming = 0;
+  let later = 0;
+
+  for (let i = 0; i < deadlines.length; i++) {
+      let dl = deadlines[i];
+      if (dl < day+1) {
+          today++;
+      } else if (dl >= day+1 && dl <= day+7) {
+          upcoming++;
+      } else if (dl > day+7 ) {
+          later++;
+      }
+  }
+
+  return [today, upcoming, later];
+}
+
+
+
+/*
+Tried this with a for-in loop first, and kept getting 1 to many in today and 1 to few in upcoming. Why would this be?
+*/
